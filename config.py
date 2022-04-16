2c0479a53f7b32ad910ee6f38b00909265052676
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-04-16 15:16:39 -0700

    Update config.py

--- config.py
+++ config.py
@@ -19,5 +19,5 @@ contact_filter = filters.create(
     or message.outgoing
 )
 
-bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Musicjmthon"))
+bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="Musicjepthon"))
 call_py = PyTgCalls(bot)
