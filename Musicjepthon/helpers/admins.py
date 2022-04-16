bd3823fe729ca760374d2797b4531848024f8ec8
جيبثون العربي <96865194+lMl10l@users.noreply.github.com>
2022-04-16 15:27:28 -0700

    Update admins.py

--- Musicjepthon/helpers/admins.py
+++ Musicjepthon/helpers/admins.py
@@ -2,8 +2,8 @@ from typing import List
 
 from pyrogram.types import Chat
 
-from Musicjmthon.helpers.get_admins import get as gett
-from Musicjmthon.helpers.get_admins import set
+from Musicjepthon.helpers.get_admins import get as gett
+from Musicjepthon.helpers.get_admins import set
 
 
 async def get_administrators(chat: Chat) -> List[int]:
